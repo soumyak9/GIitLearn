6d2ed9d37ce80a116d6e6fc66b0d858e2777fc94
soumy <soumy@10.0.0.127>
2019-04-04 18:32:54 -0700

    commit test.java changes1

--- org.GitLearn.com/src/main/java/org/GitLearn/com/org/GitLearn/com/App.java
+++ org.GitLearn.com/src/main/java/org/GitLearn/com/org/GitLearn/com/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("line1");
+        System.out.println("line2");
     }
 }
---
+++ org.GitLearn.com/src/main/java/org/GitLearn/com/org/GitLearn/com/Test.java
@@ -0,0 +1,11 @@
+package org.GitLearn.com.org.GitLearn.com;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+
+	}
+
+}
